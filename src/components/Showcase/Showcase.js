37977046518111f6b358cabebeef9d218b8830ce
HM <95879255+HMser@users.noreply.github.com>
2023-06-11 19:47:21 +0530

    Malayalam added

--- src/components/Showcase/Showcase.js
+++ src/components/Showcase/Showcase.js
@@ -20,7 +20,7 @@ function Showcase(props) {
     const [charts, setCharts] = useState([])
 
     const searchFromId = async (id) => {
-        let raw_resp = await fetch(`https://jiosaavn-api-codyandersan.vercel.app/songs?id=${id}`)
+        let raw_resp = await fetch(`https://down-spot.vercel.app/songs?id=${id}`)
         let resp = await raw_resp.json()
         props.setDetails(resp.data[0])
         navigate("/listen")
@@ -30,7 +30,7 @@ function Showcase(props) {
      * Main function that fetches homepage API sets the homepage data
      */
     const setHomepageData = async () => {
-        let uri = "https://jiosaavn-api-codyandersan.vercel.app/modules?language=hindi"
+        let uri = "https://down-spot.vercel.app/modules?language=malayalam"
 
         props.setProgress(30)
         let data = await fetch(uri)
@@ -109,7 +109,7 @@ function Showcase(props) {
 
     useEffect(() => {
         document.body.scrollTop = document.documentElement.scrollTop = 0; //scroll to top of page
-        document.title = "Popular Now - TuneStation"
+        document.title = "Popular Now - SongPedia"
         setHomepageData()
     }, [])
 
