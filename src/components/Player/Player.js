d786d09c7a3f5c39a74ab05fba99e2e57b6ae448
HM <95879255+HMser@users.noreply.github.com>
2023-06-12 18:56:10 +0530

    new method tryy

--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -1,28 +1,52 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState, useRef } from 'react'
 import { useNavigate } from "react-router-dom";
 
 function Player(props) {
-  const downloadSong = async () => {
-    props.showAlert(`Downloading ${props.details.name.replace(/&quot;/g, '"')}...`);
-    props.setProgress(10);
-
-    const url = props.details.downloadUrl[4]["link"];
-    const filename = props.details.name.replace(/&quot;/g, '"') + ` - ${props.details.primaryArtists.split(',')[0]}.m4a`;
-
-    const anchor = document.createElement('a');
-    anchor.href = url;
-    anchor.download = filename;
-    document.body.appendChild(anchor);
-    anchor.click();
-    document.body.removeChild(anchor);
-
-    props.setProgress(100);
-  };
+    // Download Functions 
+    /**
+     * Helper function for downloadSong
+     * @param {} blob The blobURL of the song.
+     * @param {string} filename The name with which to download the file.
+     */
+    const downloadBlob = (blob, filename) => {
+        var a = document.createElement('a');
+        a.download = filename;
+        a.href = blob;
+        document.body.appendChild(a);
+        props.setProgress(90)
+        a.click();
+        a.remove();
+        props.setProgress(100)
+    }
 
     /**
      * Starts downloading the song from link sent in props.
      */
-    
+    const downloadSong = async () => {
+        if (!confirm("Please confirm that you understand and agree not to distribute or share the song you are downloading, and not to engage in any form of piracy.\nPlease note that all rights of the song belong to the respective labels and/or JioSaavn, and SongPedia will not be responsible if you are found to be engaged in any form of piracy.\nBy proceeding with the download, you acknowledge and agree to these terms and those stated in the Terms of Use.")) return false
+
+        props.showAlert(`Downloading ${props.details.name.replace(/&quot;/g, '"')}...`)
+        props.setProgress(10)
+
+        const url = props.details.downloadUrl[4]["link"]
+
+        const filename = props.details.name.replace(/&quot;/g, '"') + ` - ${props.details.primaryArtists.split(',')[0]}.m4a`
+
+
+
+        const response = await fetch(url)
+
+        props.setProgress(50)
+        const blob = await response.blob()
+
+        props.setProgress(70)
+        let blobUrl = window.URL.createObjectURL(blob);
+        downloadBlob(blobUrl, filename);
+
+    }
+
+
+
     const navigate = useNavigate(); //for navigating to /search if details are not supplied
 
     const [isPlaying, setIsPlaying] = useState(false);
@@ -35,7 +59,7 @@ function Player(props) {
             navigate("/search")
             return
         }
-        document.title = `Playing ${props.details.name.replace(/&quot;/g, '"')} - TuneStation`
+        document.title = `Playing ${props.details.name.replace(/&quot;/g, '"')} - SongPedia`
         const audio = audioRef.current;
         document.getElementById("player").scrollIntoView(true)
         audio.addEventListener('loadedmetadata', () => {
@@ -64,11 +88,11 @@ function Player(props) {
     const handlePlayPause = () => {
         const audio = audioRef.current;
         if (isPlaying) {
-            document.title = `Paused ${props.details.name.replace(/&quot;/g, '"')} - TuneStation`
+            document.title = `Paused ${props.details.name.replace(/&quot;/g, '"')} - SongPedia`
             audio.pause();
             setIsPlaying(false);
         } else {
-            document.title = `Playing ${props.details.name.replace(/&quot;/g, '"')} - TuneStation`
+            document.title = `Playing ${props.details.name.replace(/&quot;/g, '"')} - SongPedia`
             audio.play();
             setIsPlaying(true);
         }
@@ -184,12 +208,13 @@ function Player(props) {
 
                                     </div>
                                     Download
-                                   <button className="focus:outline-none pr-4 group" id="downloadBtn" onClick={downloadSong}>
-        <svg className="w-4 h-4 group-hover:dark:text-white group-hover:text-gray-800 " viewBox="0 0 24 24" fill="none"
-          stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-          <path d="M3 15v4c0 1.1.9 2 2 2h14a2 2 0 0 0 2-2v-4M17 9l-5 5-5-5M12 12.8V2.5" />
-        </svg>
-      </button>                           </li>
+                                    <button className="focus:outline-none pr-4 group" id="downloadBtn" onClick={downloadSong}>
+                                        <svg className="w-4 h-4 group-hover:dark:text-white group-hover:text-gray-800 " viewBox="0 0 24 24" fill="none"
+                                            stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                                            <path d="M3 15v4c0 1.1.9 2 2 2h14a2 2 0 0 0 2-2v-4M17 9l-5 5-5-5M12 12.8V2.5" />
+                                        </svg>
+                                    </button>
+                                </li>
                                 <li className="h-auto text-right text-lg pt-2">
                                     <p className="pr-8 pb-2" id="copyrights_label">{props.details.copyright}</p>
 
