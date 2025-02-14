d0db5e6ae817933d8c2f174a073614159c962e07
hshrx <hshr.dev@gmail.com>
2025-02-14 22:33:08 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,18 +4,33 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Analytics } from '@vercel/analytics/react';
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+// 🔹 Your Firebase Configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBUT22FRcc4AccKTefp0f749wKFsnXK9P8",
+  authDomain: "songpediahm.firebaseapp.com",
+  projectId: "songpediahm",
+  storageBucket: "songpediahm.firebasestorage.app",
+  messagingSenderId: "301125407896",
+  appId: "1:301125407896:web:c33b13d5d3c78c3577aced",
+  measurementId: "G-JC71HCV06N"
+};
+
+
+// 🔹 Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-<Analytics />
+    <React.StrictMode>
+      <App auth={auth} />
+    </React.StrictMode>
+    <Analytics />
   </>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
