43e055175f8ebbbb2956437e08eae2f9d82e2885
ItIsHM <92624437+ItIsHM@users.noreply.github.com>
2025-02-16 21:29:01 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -76,6 +76,12 @@ function App() {
 
     if (loading) {
         return (
+            <div className="flex justify-center items-center h-screen space-x-2">
+    <div className="w-3 h-3 bg-blue-500 rounded-full animate-bounce"></div>
+    <div className="w-3 h-3 bg-blue-500 rounded-full animate-bounce delay-150"></div>
+    <div className="w-3 h-3 bg-blue-500 rounded-full animate-bounce delay-300"></div>
+</div>
+            
             <div className="flex justify-center items-center h-screen">
                 <p>Loading...</p> {/* Prevents flicker */}
             </div>
