5d09645452419405e85024dbf3f9206036d4656c
ItIsHM <92624437+ItIsHM@users.noreply.github.com>
2025-02-16 21:35:39 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -77,9 +77,9 @@ function App() {
     if (loading) {
         return (
             <div className="flex justify-center items-center h-screen space-x-2">
-    <div className="w-3 h-3 bg-blue-500 rounded-full animate-bounce"></div>
-    <div className="w-3 h-3 bg-blue-500 rounded-full animate-bounce delay-150"></div>
-    <div className="w-3 h-3 bg-blue-500 rounded-full animate-bounce delay-300"></div>
+    <div className="w-3 h-3 bg-green-400 rounded-full animate-bounce"></div>
+    <div className="w-3 h-3 bg-green-400 rounded-full animate-bounce delay-150"></div>
+    <div className="w-3 h-3 bg-green-400 rounded-full animate-bounce delay-300"></div>
 </div>
             
             
